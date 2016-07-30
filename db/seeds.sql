cac6865a4c00658c454349cfd2a1c3b5d4cde990
Randall Ward <randallwward@gmail.com>
2016-07-30 14:10:34 -0500

    seeds

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('Cheese Burger', FALSE, now());
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('Bacon Cheese Burger', FALSE, now());
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('Super Delux Bacon n Egg Cheese Burger', FALSE, now());
+	
